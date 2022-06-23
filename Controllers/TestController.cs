f8e0c8de5cba352550c6e7961b767a64c74d6832
kylehe1 <kylehe1@gmail.com>
2022-06-23 11:27:04 -0700

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        //Get: /Test/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //GET: /Test/Welcome?name=Kyle&numtimes=4
+        public IActionResult Welcome (string name, int numTimes =1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplicationMVC.Data;
+using System;
+using System.Linq;
+
+namespace WebApplicationMVC.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WebApplicationMVCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<WebApplicationMVCContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movies.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movies.AddRange(
+                    new Movies
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Movies
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "PG-13",
+                        Price = 8.99M
+                    },
+
+                    new Movies
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "PG-13",
+                        Price = 9.99M
+                    },
+
+                    new Movies
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "PG",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
